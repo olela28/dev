d38cd03772bda50234eefbe5db31f0fdf59152b0
olela28 <130731715+olela28@users.noreply.github.com>
2025-02-01 11:48:40 +0000

    interpreter

---
+++ interpreter/interpreter.py
@@ -0,0 +1,12 @@
+x,y,z = input("Expression: ").split(" ")
+x = float(x)
+z = float(z)
+
+if y == "+":
+    print(x + z)
+elif y == "-":
+    print(x - z)
+elif y == "/":
+    print(x / z)
+else:
+    print(x * z)
